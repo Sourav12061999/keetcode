eadf9c6d78bdc327b51e99eff1f0b1fb7d4f1c39
Sourav12061999 <sdunity12061999@gmail.com>
2023-08-06 12:11:17 +0530

    Testcases APIs Added

---
+++ server/src/Routes/Testcases/index.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { AuthMiddleware } from "../../middleware";
+import { TestcaseModel } from "../../Schemas";
+
+const router = Router();
+
+router.get("/:courseID", AuthMiddleware(), async (req, res) => {
+  const data = await TestcaseModel.find({ Ques_id: req.params.courseID });
+  res.status(200).json({
+    isError: false,
+    data,
+  });
+});
+router.post("/:courseID", AuthMiddleware(), async (req, res) => {
+  const { Input, Output, Timelimit, Ques_id } = req.body;
+  const data = await TestcaseModel.create({
+    Input,
+    Output,
+    Timelimit,
+    Ques_id,
+  });
+  res.status(200).json({
+    isError: false,
+    data,
+  });
+});
+
+router.put("/:testcaseID", AuthMiddleware(), async (req, res) => {
+  const { Input, Output, Timelimit, Ques_id } = req.body;
+
+  const data = await TestcaseModel.findByIdAndUpdate(req.params.testcaseID, {
+    Input,
+    Output,
+    Timelimit,
+    Ques_id,
+  });
+
+  res.status(200).json({
+    isError: false,
+    data,
+  });
+});
